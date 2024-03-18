d92e073d54f605dd1bac72755b7adcff345f83a1
dayul <dyulchoo@naver.com>
2024-03-18 11:17:27 +0900

    ã복ì소수 표현

--- number1.py
+++ number1.py
@@ -1,3 +1,5 @@
+from builtins import type
+
 print(0b110011100)  # 2진수
 print(0o130)    # 8진수
 print(0xD7A)    # 16진수
@@ -6,3 +8,14 @@ print(type(0o130))  # <class 'int'>
 
 print(10e3)     # 100.0 * 10^3
 print(1.23456E2)
+
+a = 8 + 24j     # 복소수 표현
+print(a)
+print(type(a))  # <class 'complex'>
+print(a.real)   # 실수 부분 표현
+print(a.imag)   # 허수 부분 표현
+print(a.conjugate())    # 켤레 복소수 구함
+
+d = 1j
+print(d * d.conjugate())    # 실수 부분만 남김
+
